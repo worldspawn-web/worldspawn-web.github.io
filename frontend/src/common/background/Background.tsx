e48d975c21ad0f3fa1a7f696b4ba20a8e8c75f8d
Worldspawn <worldspawn.web@gmail.com>
2024-02-16 19:54:00 +0300

    feat: dynamic background

---
+++ frontend/src/common/background/Background.tsx
@@ -0,0 +1,10 @@
+import styles from './Background.module.scss';
+
+export const Background = () => (
+  <div className={styles.background}>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+);
