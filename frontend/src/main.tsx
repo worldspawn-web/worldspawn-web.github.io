73e15272da58035b6288bffef9bb5a2efa1a5632
Worldspawn <worldspawn.web@gmail.com>
2024-02-19 16:24:38 +0300

    feat: data provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from './App.tsx';
+import './index.css';
+import { DataProvider } from './api/DataContext.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <DataProvider>
+      <App />
+    </DataProvider>
+  </React.StrictMode>
+);
